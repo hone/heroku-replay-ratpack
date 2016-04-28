c8a8ee94ca1da26f2808f0d23506fafb2a6ea330
Terence Lee <hone02@gmail.com>
2016-04-28 15:38:02 -0500

    fix redis metrics consumer

--- lib/consumers/metrics.rb
+++ lib/consumers/metrics.rb
@@ -7,20 +7,16 @@ require_relative '../route_metrics'
 require_relative '../kafka_options'
 
 consumer = Kafka.new(KafkaOptions.default).consumer(group_id: "metrics")
-consumer.subscribe("router", default_offset: :earliest)
+consumer.subscribe("router", default_offset: :latest)
 
 redis   = Redis.new(url: ENV['REDIS_URL'])
 metrics = RouteMetrics.new(redis)
 
 puts "Consuming"
 
-consumer.each_batch do |batch|
-  consumer.stop
-  puts batch.messages.count
-  batch.messages.each do |message|
-    json  = JSON.parse(message.value)
-    route = Route.new(json)
-    puts "Processing: #{route.path}"
-    metrics.insert(route)
-  end
+consumer.each_message do |message|
+  json  = JSON.parse(message.value)
+  route = Route.new(json)
+  puts "Processing: #{message.offset} #{route.path}"
+  metrics.insert(route)
 end
